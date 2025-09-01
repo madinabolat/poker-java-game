ebb23d034efbae769504779b8518b3adec53eb70
madinabolat <madinabolat01@gmail.com>
2025-08-31 21:28:20 -0400

    config

--- poker/src/main/java/Deck/Card.java
+++ poker/src/main/java/Deck/Card.java
@@ -3,4 +3,9 @@ package Deck;
 public class Card {
     public Rank rank;
     public Suit suit;
+
+    public Card(Rank rank, Suit suit){
+        this.rank = rank;
+        this.suit = suit;
+    }
 }
--- poker/src/main/java/org/example/Main.java
+++ poker/src/main/java/org/example/Main.java
@@ -4,6 +4,8 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Card card = new Card("TWO", "HEARTS");
+        //how to pass string to enum?
 
     }
 }
