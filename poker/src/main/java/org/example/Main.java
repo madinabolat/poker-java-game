479d65ea2ec9a6fb297dab9d3040eac9608dfae1
madinabolat <madinabolat01@gmail.com>
2025-09-06 21:21:11 -0400

    override equals method

--- poker/src/main/java/org/example/Main.java
+++ poker/src/main/java/org/example/Main.java
@@ -8,10 +8,15 @@ public class Main {
     public static void main(String[] args) {
         Card card1 = new Card(Rank.TWO, Suit.CLUBS);
         Card card2 = new Card(Rank.TWO, Suit.CLUBS);
-        System.out.println(card1);
-        System.out.println(card2);
+//        System.out.println(card1);
+//        System.out.println(card2);
+//        System.out.println(card1==card2);
+//        System.out.println(card1==card1);
+//        System.out.println(card1.rank.equals(card2.rank));
+//        System.out.println(card1.rank.equals(card2.rank));
+        System.out.println(card1.equals(card2));
         System.out.println(card1==card2);
-        System.out.println(card1==card1);
+
 
     }
 }
