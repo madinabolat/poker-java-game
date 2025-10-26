3ee15d3a7ce6de04a3e23d24c498e7d7a7b64309
madinabolat <madinabolat01@gmail.com>
2025-10-25 23:40:19 -0400

    upd

--- poker/src/main/java/org/example/PokerApp.java
+++ poker/src/main/java/org/example/PokerApp.java
@@ -13,17 +13,7 @@ import java.util.Comparator;
 
 public class PokerApp {
     public static void main(String[] args) {
-//        Deck deck = new Deck();
-//        deck.display();
-//        deck.shuffle();
-//        System.out.println("After shuffling");
-//        deck.display();
-//
-//        Dealer pokerHand = new Dealer();
-//        pokerHand.dealHands();
-//        pokerHand.displayCommunityCards();
-//
-//        pokerHand.getCommunityCards().sort(Comparator.comparing(Card::getRank));
+
 
         ArrayList<Card> testDeck = new ArrayList<Card>();
         testDeck.add(new Card(Rank.TEN, Suit.DIAMONDS));
@@ -33,14 +23,6 @@ public class PokerApp {
         testDeck.add(new Card(Rank.EIGHT, Suit.DIAMONDS));
         HandEvaluator handEvaluator = new HandEvaluator();
         System.out.println(handEvaluator.determineRank(testDeck));
-
-
-
-//        System.out.println("After sorting");
-//        pokerHand.displayCommunityCards();
-//
-//        System.out.println(Rank.ACE.ordinal());
-//        System.out.println(Rank.TEN.ordinal());
 
     }
 }
