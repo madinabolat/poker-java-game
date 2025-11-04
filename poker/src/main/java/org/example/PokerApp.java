a58550dcbe075dd78f182343916bef8cc72077aa
madinabolat <madinabolat01@gmail.com>
2025-11-03 22:08:02 -0500

    test

--- poker/src/main/java/org/example/PokerApp.java
+++ poker/src/main/java/org/example/PokerApp.java
@@ -14,19 +14,32 @@ import java.util.Comparator;
 public class PokerApp {
     public static void main(String[] args) {
 
-        Dealer dealer = new Dealer();
-        dealer.dealHands();
-        dealer.displayCommunityCards();
-        dealer.determineWinner();
-//
-//        ArrayList<Card> testDeck = new ArrayList<Card>();
-//        testDeck.add(new Card(Rank.TEN, Suit.DIAMONDS));
-//        testDeck.add(new Card(Rank.NINE, Suit.DIAMONDS));
-//        testDeck.add(new Card(Rank.SEVEN, Suit.DIAMONDS));
-//        testDeck.add(new Card(Rank.SIX, Suit.DIAMONDS));
-//        testDeck.add(new Card(Rank.EIGHT, Suit.DIAMONDS));
-//        HandEvaluator handEvaluator = new HandEvaluator();
-//        System.out.println(handEvaluator.determineRank(testDeck));
+//        Dealer dealer = new Dealer();
+//        dealer.dealHands();
+//        dealer.displayCommunityCards();
+//        dealer.determineWinner();
 
+
+
+        ArrayList<Card> testDeck = new ArrayList<Card>();
+        testDeck.add(new Card(Rank.TEN, Suit.DIAMONDS));
+        testDeck.add(new Card(Rank.NINE, Suit.DIAMONDS));
+        testDeck.add(new Card(Rank.SEVEN, Suit.DIAMONDS));
+        testDeck.add(new Card(Rank.SIX, Suit.DIAMONDS));
+        testDeck.add(new Card(Rank.ACE, Suit.DIAMONDS));
+        testDeck.add(new Card(Rank.QUEEN, Suit.DIAMONDS));
+        testDeck.add(new Card(Rank.KING, Suit.DIAMONDS));
+        HandEvaluator handEvaluator = new HandEvaluator();
+        ArrayList<ArrayList<Card>> subsets = new ArrayList<>();
+        subsets = handEvaluator.generateKCardCombinations(testDeck,5);
+        int counter = 1;
+        for (ArrayList<Card> subset : subsets){
+            System.out.println();
+            System.out.println("Subset " + counter + ": ");
+            for (Card card : subset){
+                System.out.print(card.rank + " " + card.suit + ", ");
+            }
+            counter ++;
+        }
     }
 }
